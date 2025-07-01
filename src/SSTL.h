6a8fce4c1dc01ec83f5ab88fdecaf0bacd7a6a6e
Claw <clawschwic001@monocles.eu>
2025-06-30 22:53:04 -0500

    Changed value type

--- src/SSTL.h
+++ src/SSTL.h
@@ -3,16 +3,16 @@
 
 #include <stdbool.h>
 
-#define ushort unsigned short
+#define ulong unsigned long long
 
 enum SSTL_TextStyle { Reset, Bold, Faint, Italic, Underline };
 enum SSTL_TextColor { Black, Red, Green, Yellow, Blue, Magenta, Cyan, White };
 
-ushort SSTL_Clear( );
+ulong SSTL_Clear( );
 
-ushort SSTL_Position( ushort X, ushort Y );
+ulong SSTL_Position( ulong X, ulong Y );
 
-ushort SSTL_ForegroundColor( bool Intensity, enum SSTL_TextColor );
-ushort SSTL_BackgroundColor( bool Intensity, enum SSTL_TextColor );
+ulong SSTL_ForegroundColor( bool Intensity, enum SSTL_TextColor );
+ulong SSTL_BackgroundColor( bool Intensity, enum SSTL_TextColor );
 
 #endif
